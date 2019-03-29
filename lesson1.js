2d2cb7a24d2c440ddb8b22c04bc374a5c831cf82
Alexey Nikolaevskiy <anikolaevski@gmail.com>
2019-03-29 22:22:12 +0300

    Lesson1, Lesson2

---
+++ lesson1.js
@@ -0,0 +1,17 @@
+// Task#1
+let x =2 ;
+let y = 22;
+let z = 0;
+let result = x * y + 5 * z + x - 1;
+console.log(result);
+
+//Task #2
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+let result_2 = (algebra + geography + physics) / 3;
+console.log(result_2);
+
+//Task#3
+let name = 'Алексей';
+console.log(`Привет, мир! Меня зовут ${name}`);
---
+++ lesson2.js
@@ -0,0 +1,83 @@
+// Task#1 v.1
+"use strict";
+function getResult(a, b, c) {
+  let discr = b ** 2 - 4 * a * c;
+  let result = [];
+  if (discr > 0) {
+    result.unshift((-b - Math.sqrt(discr)) / (2 * a));
+    result.unshift((-b + Math.sqrt(discr)) / (2 * a));
+  } else if (discr == 0) {
+    result.unshift(-b / (2 * a));
+  } else {
+    console.log('Нет корней!');
+  }
+  return result;
+}
+console.log(getResult(2, 4, -3));
+
+// Task#1 v.2
+"use strict";
+function getResult(a, b, c) {
+  let discr = b ** 2 - 4 * a * c;
+  let result = [];
+  if (discr > 0) {
+    result.unshift((-b - Math.sqrt(discr)) / (2 * a));
+    result.unshift((-b + Math.sqrt(discr)) / (2 * a));
+  } else if (discr == 0) {
+    result.unshift(-b / (2 * a));
+  }
+  return result;
+}
+let res = getResult(2, 4, -3);
+if (res.length > 0) {
+  console.log(res);
+} else {
+  console.log('Нет корней!');
+}
+console.log(getResult(2, 4, -3));
+
+//Task#2
+"use strict";
+function askDrink() {
+  let name = prompt('Введите Ваше имя');
+  let dateOfBirthday = new Date(prompt('Введите дату Вашего рождения'));
+  let curDate = new Date();
+  let curYear = curDate.getFullYear();
+  let birthYear = dateOfBirthday.getFullYear();
+  let age = curYear - birthYear;
+  console.log(name, dateOfBirthday, age);
+  if(age >= 18) {
+    result = confirm(`Не желаете ли олд-фэшн, ${name}?`);
+   } else {
+    alert(`Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!”`)
+   }
+}
+askDrink();
+
+//Task#3
+"use strict";
+function getAverageMark(marks) {
+  let resultSum = 0;
+  for (let i = 0; i < marks.length; i++) {
+    resultSum += parseInt(marks[i], 10);
+  }
+  return (marks.length)?resultSum / marks.length:0;
+}
+// unit test:
+console.log(getAverageMark([1,2,3,4,5]));
+
+//Main code
+let dataLimit, marksStr, marks, resultNbr, result;
+dataLimit = 5;
+marksStr = prompt('Введите полученные оценки');
+marks = marksStr.split(' ');
+resultNbr = marks.length;
+if (resultNbr > dataLimit) { 
+  alert(`Внимание! Вы ввели ${resultNbr} оценок. Предельное количество оценок = ${dataLimit}.\nВ расчет будут взяты первые ${dataLimit} оценок.`);
+  for (let i = resultNbr; i > dataLimit; --i ) {
+    marks.pop();
+  }
+}
+result = getAverageMark(marks);
+console.log(`Введены оценки: ${marks}`);
+console.log(`Средняя оценка: ${result}`);
